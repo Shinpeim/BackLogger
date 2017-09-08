8106b9594304ea558ce09b55407f7c051ce0ffaf
shinpei maruyama <shinpeim@gmail.com>
2017-09-08 22:05:31 +0900

    Setup electron

---
+++ main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, "web", "build", 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
