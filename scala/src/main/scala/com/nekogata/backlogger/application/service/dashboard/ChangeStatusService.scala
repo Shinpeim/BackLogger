99a68edc5a86edc29ff808b14c4327992b56d370
shinpei maruyama <shinpeim@gmail.com>
2017-09-09 23:47:07 +0900

    「処理中にする」実装

--- scala/src/main/scala/com/nekogata/backlogger/application/service/dashboard/ChangeStatusService.scala
+++ scala/src/main/scala/com/nekogata/backlogger/application/service/dashboard/ChangeStatusService.scala
@@ -1,17 +1,35 @@
 package com.nekogata.backlogger.application.service.dashboard
 
 import com.nekogata.backlogger.domain.issue.IssueRepository
+import com.nekogata.backlogger.domain.setting.SettingRepository
+import com.nekogata.backlogger.infrastructure.api_client.BackLogApiClient
 import com.nekogata.backlogger.infrastructure.issue.IssueRepositoryImpl
+import com.nekogata.backlogger.infrastructure.setting.SettingRepositoryImpl
 import com.nekogata.backlogger.js_exports.events.IssueEvents
+
+import scala.util.{Failure, Success}
 
 class ChangeStatusService {
   val issueRepository: IssueRepository = new IssueRepositoryImpl
+  val settingRepository: SettingRepository = new SettingRepositoryImpl
+
+  import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
   def makeStatusAsProcessing(projectId: Int, issueId: Int): Unit = {
     val oldIssue = issueRepository.get(projectId, issueId)
     val issue = oldIssue.makeStatusProcessing()
     issueRepository.store(projectId, issue)
-
     IssueEvents.repositoryChanged.fire()
+
+    val setting = settingRepository.get()
+    val client = new BackLogApiClient(setting.spaceName, setting.apiKey)
+    client.changeIssueStatusToProcessing(projectId, issueId).onComplete {
+      case Success(_) =>
+        issueRepository.store(projectId, issue.commitSynchronizing)
+        IssueEvents.repositoryChanged.fire()
+      case Failure(_) =>
+        issueRepository.store(projectId, oldIssue)
+        IssueEvents.repositoryChanged.fire()
+    }
   }
 }
